cb3ce906d6846655321a865df8f8a1aedf15720c
kojisato118 <k.s.9190118@gmail.com>
2016-06-18 22:43:50 +0900

    Initial Commit

---
+++ BackgroundFetcher/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BackgroundFetcher
+//
+//  Created by 佐藤 康次 on 2016/06/18.
+//  Copyright © 2016年 佐藤 康次. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
